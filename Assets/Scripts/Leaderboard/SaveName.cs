028a48a0434e8b3629c3edb10c83654b6d3ccf3e
mmwxxy <mmwxxy@gmail.com>
2022-09-20 03:04:14 +0700

    savescore in local list

---
+++ Assets/Scripts/Leaderboard/SaveName.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveName : MonoBehaviour
+{
+    public InputField textBox;
+    // public Text highscoretext;
+
+    int count;
+    int getcount;
+
+    public void clickSaveButton(){
+
+        getcount=PlayerPrefs.GetInt("savecount");
+        PlayerPrefs.SetString("name", textBox.text);
+        Debug.Log("Your Name is " + PlayerPrefs.GetString("name"));                
+        PlayerPrefs.SetInt("savecount", getcount);
+        Debug.Log("Count Success" + PlayerPrefs.GetInt("savecount"));
+        getcount++;
+        PlayerPrefs.SetInt("savecount", getcount);
+
+    }
+
+}
